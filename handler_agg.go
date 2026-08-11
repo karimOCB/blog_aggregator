ea0be411c6c5ec321731fe83c17768479cab8e05
karim <chaya.karim.omar@gmail.com>
2026-08-11 22:03:01 +0000

    AU: Posts

--- handler_agg.go
+++ handler_agg.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"strings"
 	"time"
+	"strconv"
+
+	"github.com/google/uuid"
+	"github.com/karimOCB/blog_aggregator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
@@ -27,6 +33,32 @@ func handlerAgg(s *state, cmd command) error {
 }
 
 
+func handlerBrowse(s *state, cmd command, user database.User) error {
+	var limit int32 = 2
+	if len(cmd.Args) != 0 {
+		number, err := strconv.Atoi(cmd.Args[0])
+		if err != nil {
+			return fmt.Errorf("couldn't parse limit argument given: %w", err)
+		}
+		limit = int32(number)
+	}
+
+	posts, err := s.db.GetPostsForUser(context.Background(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit: limit,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't get posts for user: %w", err)
+	}
+
+	for _, post := range posts {
+		fmt.Printf("Post: %+v\n", post)
+	}
+
+	return nil
+}
+
+
 func scrapeFeeds(s *state) error {
 	feed, err := s.db.GetNextFeedToFetch(context.Background())
 	
@@ -44,10 +76,41 @@ func scrapeFeeds(s *state) error {
 		return fmt.Errorf("could not fetch rssfeed: %w", err)
 	}
 
-	fmt.Println("\nNext Feed: ")
-
 	for _, item := range rssfeed.Channel.Item {
-		fmt.Printf("item title: %s \n", item.Title)
+		pubDate := sql.NullTime{
+			Valid: false,
+		}
+		
+		parsedPubDate, err := time.Parse(time.RFC1123, item.PubDate)
+		if err == nil {
+			pubDate.Valid = true
+			pubDate.Time = parsedPubDate
+		} 
+		
+		err = s.db.CreatePost(context.Background(), database.CreatePostParams{
+			ID: uuid.New(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			Title: sql.NullString{
+				String: item.Title,
+				Valid: item.Title != "",
+			},
+			Url: item.Link,
+			Description: sql.NullString{
+				String: item.Description,
+				Valid: item.Description != "",
+			},
+			PublishedAt: pubDate,
+			FeedID: feed.ID,
+		})
+		
+		if err != nil {
+			if strings.Contains(err.Error(), "violates unique constraint") {
+				continue
+			} else {
+				fmt.Printf("couldn't create post: %s", err)
+			}
+		}
 	}
 
 	return nil
--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -10,3 +10,12 @@ VALUES(
     $7,
     $8    
 );
+
+-- name: GetPostsForUser :many
+SELECT posts.*, feeds.name AS feed_name
+FROM posts
+INNER JOIN feed_follows ON feed_follows.feed_id = posts.feed_id
+INNER JOIN feeds ON feeds.id = posts.feed_id
+WHERE feed_follows.user_id = $1
+ORDER BY posts.published_at DESC
+LIMIT $2;
